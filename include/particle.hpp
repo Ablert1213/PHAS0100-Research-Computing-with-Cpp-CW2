b1b522ce995b07574e4a410af703b420cf355ff2
Albert Yin <ucapjyi@ucl.ac.uk>
2023-03-26 16:22:48 +0000

    creating the particle class that holds a particle's inputs

--- include/particle.hpp
+++ include/particle.hpp
@@ -1,8 +1,33 @@
-class Particle {
+#include <Eigen/Dense>
+
+// class Particle {
+//     public:
+//         Particle(double in_mass);
+//         double getMass() const;
+
+//     private:
+//         double mass;
+// };
+
+namespace n_body 
+{
+class Particle{
     public:
-        Particle(double in_mass);
+        Particle(const Eigen::Vector3d& position, const Eigen::Vector3d& velocity, double mass);
+        
+        Eigen::Vector3d getPosition() const;
+        Eigen::Vector3d getVelocity() const;
+        Eigen::Vector3d getAcceleration() const;
+
         double getMass() const;
+        void update(double dt, const Eigen::Vector3d& acceleration);
 
     private:
-        double mass;
+        Eigen::Vector3d position_;
+        Eigen::Vector3d velocity_;
+        Eigen::Vector3d acceleration_;
+
+        double mass_;
+
 };
+}
--- src/particle.cpp
+++ src/particle.cpp
@@ -1,10 +1,41 @@
 #include "particle.hpp"
 #include <Eigen/Core>
 
-Particle::Particle(double in_mass) :
-    mass{in_mass}
+#include <iostream>
+
+// Particle::Particle(double in_mass) :
+//     mass{in_mass}
+//     {}
+
+// double Particle::getMass() const {
+//     return mass;
+// }
+
+namespace n_body 
+{
+Particle::Particle(const Eigen::Vector3d& position, const Eigen::Vector3d& velocity, double mass)
+    : position_ (position), velocity_ (velocity), mass_ (mass)
     {}
 
+Eigen::Vector3d Particle::getPosition() const {
+    return position_;
+}
+
+Eigen::Vector3d Particle::getVelocity() const {
+    return velocity_;
+}
+
+Eigen::Vector3d Particle::getAcceleration() const {
+    return acceleration_;
+}
+
 double Particle::getMass() const {
-    return mass;
+    return mass_;
+}
+
+void Particle::update(double dt, const Eigen::Vector3d& acceleration) {
+    velocity_ += dt * acceleration_;
+    position_ += dt * velocity_;
+}
+
 }
